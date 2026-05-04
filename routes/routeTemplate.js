088f1452e5f3e44f6bce5cfb75aba68e3445d415
James G <jgarcia97@my.bcit.ca>
2026-05-04 15:06:43 -0700

    Create a route template for future endpoints

---
+++ routes/routeTemplate.js
@@ -0,0 +1,16 @@
+// import all dependencies
+const express = require("express");
+
+// create instance of express (but with the .Router() method)
+const router = express.Router();
+
+/**
+ * Then declare all your routes under here.
+ * Keep in mind that if you create a route here. You must also modify server.js
+ * to redirect to this given a certain eyJlbWFpbCI6InNteW5vdHRAbXkuYmNpdC5jYSIsImNyZWF0ZWQiOjE3Nzc1MTc5MDM5NDEsImlhdCI6MTc3NzUxNzkwM30
+ *
+ * i.e., if you want to only be redirected here when server.js hits a /login endpoint, you must include
+ * app.get("/login", require("./routes/{file_name}"))
+ */
+
+module.exports = router;
