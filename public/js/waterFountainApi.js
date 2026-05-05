4255cb18b51eb2627272aac262ba11df83ac3e26
Luka Gajic <luka.gajic2013@gmail.com>
2026-05-05 13:37:50 -0700

    Add toggle functionality to display water fountains

--- public/js/waterFountainApi.js
+++ public/js/waterFountainApi.js
@@ -1,4 +1,9 @@
+let fountainMarkers = [];
+let fountainData = [];
+let fountainVisible = false;
+
 const fetchWaterFountains = async () => {
+    fountainData = [];
   try {
     const limit = 100;
     let offset = 0;
@@ -10,23 +15,47 @@ const fetchWaterFountains = async () => {
       const resultJSON = await result.json();
       console.log(resultJSON);
 
-      resultJSON.results.forEach((result) => {
-        let lonValue = result.geo_point_2d["lon"];
-        let latValue = result.geo_point_2d["lat"];
-        var marker = L.marker([latValue, lonValue]).addTo(map);
-
-        let fountainInfo = result.name.replace(
-          "Fountain location:\n",
-          "Water Fountain: ",
-        );
-        marker.bindPopup(fountainInfo).openPopup();
-      });
+      for (let i = 0; i < resultJSON.results.length; i++) {
+        fountainData.push(resultJSON.results[i]);
+      }
 
       if (resultJSON.results.length < limit) break;
       offset += limit;
     }
   } catch (error) {
     console.log(error);
+  }
+  createFountainMarkers();
+};
+
+const createFountainMarkers = () => {
+  fountainMarkers = [];
+  for (let i = 0; i < fountainData.length; i++) {
+    const result = fountainData[i];
+    let lonValue = result.geo_point_2d["lon"];
+    let latValue = result.geo_point_2d["lat"];
+    const marker = L.marker([latValue, lonValue]);
+    let fountainInfo = result.name.replace(
+      "Fountain location:\n",
+      "Water Fountain: ",
+    );
+    marker.bindPopup(fountainInfo);
+    fountainMarkers.push(marker);
+  }
+};
+
+export const toggleFountainMarkers = () => {
+  if (fountainVisible) {
+    fountainMarkers.forEach((marker) => {
+      map.removeLayer(marker);
+    });
+
+    fountainVisible = false;
+  } else {
+    fountainMarkers.forEach((marker) => {
+      marker.addTo(map);
+    });
+    fountainVisible = true;
   }
 };
 
