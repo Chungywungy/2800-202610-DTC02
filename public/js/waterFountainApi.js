e4e3b87c4fd721433a2563b8d28a14e7ecb52840
Luka Gajic <luka.gajic2013@gmail.com>
2026-05-05 12:42:27 -0700

    Render all water fountains on map

--- public/js/waterFountainApi.js
+++ public/js/waterFountainApi.js
@@ -1,22 +1,30 @@
 const fetchWaterFountains = async () => {
   try {
-    const result = await fetch(
-      "https://opendata.vancouver.ca/api/explore/v2.1/catalog/datasets/drinking-fountains/records?limit=100",
-    );
-    const resultJSON = await result.json();
-    console.log(resultJSON);
+    const limit = 100;
+    let offset = 0;
 
-    resultJSON.results.forEach((result) => {
-      let lonValue = result.geo_point_2d["lon"];
-      let latValue = result.geo_point_2d["lat"];
-      var marker = L.marker([latValue, lonValue]).addTo(map);
-
-      let fountainInfo = result.name.replace(
-        "Fountain location:\n",
-        "Water Fountain: ",
+    while (true) {
+      const result = await fetch(
+        `https://opendata.vancouver.ca/api/explore/v2.1/catalog/datasets/drinking-fountains/records?limit=${limit}&offset=${offset}`,
       );
-      marker.bindPopup(fountainInfo).openPopup();
-    });
+      const resultJSON = await result.json();
+      console.log(resultJSON);
+
+      resultJSON.results.forEach((result) => {
+        let lonValue = result.geo_point_2d["lon"];
+        let latValue = result.geo_point_2d["lat"];
+        var marker = L.marker([latValue, lonValue]).addTo(map);
+
+        let fountainInfo = result.name.replace(
+          "Fountain location:\n",
+          "Water Fountain: ",
+        );
+        marker.bindPopup(fountainInfo).openPopup();
+      });
+
+      if (resultJSON.results.length < limit) break;
+      offset += limit;
+    }
   } catch (error) {
     console.log(error);
   }
