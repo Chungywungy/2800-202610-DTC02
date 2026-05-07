d2bce5e992fd4e350de66b6a009ac4ff5bfd3c8c
James G <jgarcia97@my.bcit.ca>
2026-05-07 16:41:23 -0700

    Create toggleable login and registration page

---
+++ public/js/login.js
@@ -0,0 +1,9 @@
+// Toggle login and register page
+createAccountBtn.addEventListener("click", () => {
+  registerFormContainer.classList.toggle("hidden");
+  loginFormContainer.classList.toggle("hidden");
+});
+alreadyHaveAccountBtn.addEventListener("click", () => {
+  registerFormContainer.classList.toggle("hidden");
+  loginFormContainer.classList.toggle("hidden");
+});
