4081b698459e09ac5334642d3b052a3c3cdc95bc
James G <jgarcia97@my.bcit.ca>
2026-05-07 23:17:30 -0700

    Fix user session store not saving on turned off remember me checkbox

--- routes/auth.js
+++ routes/auth.js
@@ -34,13 +34,13 @@ router.post("/login", async (req, res) => {
   if (!passwordMatch) {
     return res.status(401).json({ message: "Incorrect password. Try again." });
   }
+  req.session.user = {
+    username: userFound.username,
+    email: userFound.email,
+    role: userFound.role,
+  };
   if (rememberMe) {
     req.session.cookie.maxAge = 1000 * 60 * 60 * 24 * 30; // 30 days
-    req.session.user = {
-      username: userFound.username,
-      email: userFound.email,
-      role: userFound.role,
-    };
   } else {
     req.session.cookie.expires = false; // expires when browser closes
   }
